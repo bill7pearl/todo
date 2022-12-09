512524fdb9ce6fd7a4db64934e205e12b050d9b3
Billal <billal7chami@gmail.com>
2022-12-09 19:01:36 +0100

    Fix reviewer note

--- src/index.js
+++ src/index.js
@@ -7,28 +7,29 @@ const tasks = [
   {
     description: 'workouts for 10 minutes',
     completed: false,
-    index: 1,
+    index: 2,
   },
   {
     description: 'read a book',
     completed: false,
-    index: 2,
+    index: 3,
   },
   {
     description: 'practise coding',
     completed: false,
-    index: 3,
+    index: 1,
   },
 ];
 
 const todoListContainer = document.getElementById('todo-list');
 
 let tasksList = [];
+tasks.sort((a, b) => a.index - b.index);
 for (let i = 0; i < tasks.length; i++) {
   tasksList += `
-<div class="tasks-container">
-<div class="task-description"> <input type="checkbox"> ${tasks[i].description}</div>
-</div>
-`;
+        <div class="tasks-container">
+        <div class="task-description"> <input type="checkbox"> ${tasks[i].description}</div>
+        </div>
+        `;
 }
 todoListContainer.innerHTML = tasksList;
