be3e54c9fa6e85fec202749debb3711f1b73b656
Billal <billal7chami@gmail.com>
2022-12-14 11:19:22 +0100

    Fix all linters errors

--- src/add-remove.js
+++ src/add-remove.js
@@ -1,111 +1,98 @@
-/* eslint-disable */
-//import _ from 'lodash';
+/* eslint-disable no-unused-vars */
+// import _ from 'lodash';
 import './style.css';
 
 let todos = [];
-window.addEventListener('load', () => {
-	todos = JSON.parse(localStorage.getItem('todos')) || [];
-	const nameInput = document.querySelector('#name');
-	const newTodoForm = document.querySelector('#new-todo-form');
-	newTodoForm.addEventListener('submit', e => {
-		e.preventDefault();
+function DisplayTodos() {
+  const todoList = document.querySelector('#todo-list');
+  todoList.innerHTML = '';
 
-		const todo = {
-      index: 1,
-			content: e.target.elements.content.value,
-			completed: false,
-		}
-		todos.push(todo);
+  todos.forEach((todo) => {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item');
 
-		localStorage.setItem('todos', JSON.stringify(todos));
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const content = document.createElement('div');
+    const actions = document.createElement('div');
+    const edit = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    input.type = 'checkbox';
+    input.checked = todo.completed;
+    span.classList.add('bubble');
+    content.classList.add('todo-content');
+    actions.classList.add('actions');
+    edit.classList.add('edit');
+    deleteButton.classList.add('delete');
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+    edit.innerHTML = 'Edit';
+    deleteButton.innerHTML = 'Delete';
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    actions.appendChild(deleteButton);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+    todoList.appendChild(todoItem);
 
-		e.target.reset();
-		DisplayTodos()
-	})
+    if (todo.completed) {
+      todoItem.classList.add('done');
+    }
 
-	DisplayTodos()
-})
+    input.addEventListener('change', (e) => {
+      todo.completed = e.target.checked;
+      localStorage.setItem('todos', JSON.stringify(todos));
 
-function DisplayTodos () {
-	const todoList = document.querySelector('#todo-list');
-	todoList.innerHTML = "";
+      if (todo.completed) {
+        todoItem.classList.add('done');
+      } else {
+        todoItem.classList.remove('done');
+      }
 
-	todos.forEach(todo => {
-		const todoItem = document.createElement('div');
-		todoItem.classList.add('todo-item');
+      DisplayTodos();
+    });
 
-		const label = document.createElement('label');
-		const input = document.createElement('input');
-		const span = document.createElement('span');
-		const content = document.createElement('div');
-		const actions = document.createElement('div');
-		const edit = document.createElement('button');
-		const deleteButton = document.createElement('button');
+    edit.addEventListener('click', (e) => {
+      const input = content.querySelector('input');
+      input.removeAttribute('readonly');
+      input.focus();
+      edit.innerHTML = 'Save';
+      input.addEventListener('blur', (e) => {
+        input.setAttribute('readonly', true);
+        todo.content = e.target.value;
+        localStorage.setItem('todos', JSON.stringify(todos));
+        DisplayTodos();
+      });
+    });
 
-		input.type = 'checkbox';
-		input.checked = todo.completed;
-		span.classList.add('bubble');
-		/* if (todo.category == 'personal') {
-			span.classList.add('personal');
-		} else {
-			span.classList.add('business');
-		} */
-		content.classList.add('todo-content');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
-		deleteButton.classList.add('delete');
-
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-		edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
-
-		label.appendChild(input);
-		label.appendChild(span);
-		actions.appendChild(edit);
-		actions.appendChild(deleteButton);
-		todoItem.appendChild(label);
-		todoItem.appendChild(content);
-		todoItem.appendChild(actions);
-
-		todoList.appendChild(todoItem);
-
-		if (todo.completed) {
-			todoItem.classList.add('done');
-		}
-		
-		input.addEventListener('change', (e) => {
-			todo.completed = e.target.checked;
-			localStorage.setItem('todos', JSON.stringify(todos));
-
-			if (todo.completed) {
-				todoItem.classList.add('done');
-			} else {
-				todoItem.classList.remove('done');
-			}
-
-			DisplayTodos()
-
-		})
-
-		edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-      edit.innerHTML = 'Save'
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				todo.content = e.target.value;
-				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
-
-		deleteButton.addEventListener('click', (e) => {
-			todos = todos.filter(t => t != todo);
-			localStorage.setItem('todos', JSON.stringify(todos));
-			DisplayTodos()
-		})
-
-	})
+    deleteButton.addEventListener('click', (e) => {
+      todos = todos.filter((t) => t !== todo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+      DisplayTodos();
+    });
+  });
 }
+window.addEventListener('load', () => {
+  todos = JSON.parse(localStorage.getItem('todos')) || [];
+  const nameInput = document.querySelector('#name');
+  const newTodoForm = document.querySelector('#new-todo-form');
+  newTodoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const todo = {
+      index: 1,
+      content: e.target.elements.content.value,
+      completed: false,
+    };
+    todos.push(todo);
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    e.target.reset();
+    DisplayTodos();
+  });
+
+  DisplayTodos();
+});
